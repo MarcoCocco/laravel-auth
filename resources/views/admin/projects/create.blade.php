03119ad3b40cfd39ec25933922267e4dba986499
MarcoCocco <marcococco28@gmail.com>
2023-05-22 14:04:54 +0200

    feat: creata la sezione show per vedere i progetti nel dettaglio

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts/admin')
+
+@section('content')
+<h2>Form creazione nuovo progetto</h2>
+@endsection
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts/admin')
 
 @section('content')
-<h2>Visualizzazione Progetto nel dettaglio</h2>
-@endsection
+    <div class="container">
+        <h1 class='text-center m-4'>Dettagli Progetto</h1>
+        <div class="py-3">
+            <h3 class="text-center">
+                {{ $project->title }}
+            </h3>
+            <hr>
+            <p>
+                {{ $project->description }}
+            </p>
+            <table class="mt-5 table table-striped">
+                <thead>
+                    <th>Link alla Repository</th>
+                    <th>Linguaggio</th>
+                    <th>Data di creazione</th>
+                    <th>Completo</th>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>{{ $project->github_link }}</td>
+                        <td>{{ $project->language }}</td>
+                        <td>{{ $project->creation_date }}</td>
+                        <td>{{ $project->is_complete ? 'Sì' : 'No' }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    @endsection
